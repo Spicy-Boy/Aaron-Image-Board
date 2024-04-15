ba9a7a205b222696952ca216c35106d62dce9568
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2024-04-15 11:12:56 -0400

    Pop up reply links success!

--- public/js/createReplyLinks.js
+++ public/js/createReplyLinks.js
@@ -52,26 +52,36 @@ for (let postNo of postIDs)
                 //highlight referenced post vv
                 referencedPost.classList.add("highlighted");
 
-                //create a little pop up of the post vvv
-                const popupPost = document.createElement('div');
-                
-                clonedPost = referencedPost.cloneNode(true);
-                popupPost.appendChild(clonedPost);
 
-                popupPost.classList.add("popup");
+                //create a little pop up of the post vvv if reference is outside window
+                if (!isElementVisibleInViewport(referencedPost))
+                {
+                    const popupPost = document.createElement('div');
+                    
+                    clonedPost = referencedPost.cloneNode(true);
+                    popupPost.appendChild(clonedPost);
 
-                // let linkRectangle = link.getBoundingClientRect();
-                // popupPost.style.top = linkRectangle.bottom+'px';
-                // popupPost.style.left = linkRectangle.left+'px';
-                popupPost.style.top = event.clientX+'px';
-                popupPost.style.left = event.clientY+'px';
+                    popupPost.classList.add("popup");
 
-                document.body.appendChild(popupPost);
-                console.log(popupPost);
+                    let linkRectangle = link.getBoundingClientRect();
+                    // popupPost.style.top = linkRectangle.bottom+'px';
+                    // popupPost.style.left = linkRectangle.left+'px';
+                    popupPost.style.top = (linkRectangle.top+linkRectangle.height+window.scrollY)+'px';
+                    popupPost.style.left = linkRectangle.left+'px';
+
+                    document.body.appendChild(popupPost);
+                    // console.log(popupPost);
+                }
 
             });
             link.addEventListener("mouseleave", () => {
                 referencedPost.classList.remove("highlighted");
+
+                // remove existing popup
+                const popupPost = document.querySelector('.popup');
+                if (popupPost) {
+                    popupPost.remove();
+                }
             });
             link.addEventListener("click", () => {
                 let highElements = document.querySelectorAll('.perma-highlighted');
@@ -86,22 +96,14 @@ for (let postNo of postIDs)
     }
 }
 
-// DOESNT WORK vvv
-            // //the post that the link is referencing
-            // const referencedPost = arrPostReplies.find( otherPost => otherPost.id === postNo);
-            // //TESTER vv
-            // console.log(post.id+" references "+referencedPost.id);
-
-            // modifiedLinks = post.querySelectorAll(".referencing-"+postNo);
-            // console.log("modded links:",modifiedLinks);
-            // modifiedLinks.forEach( (link) => {
-            //     console.log(link);
-            //     link.addEventListener("mouseenter", () => {
-            //         console.log('MOUSE OVER!!');
-            //         // referencedPost.classList.add("highlighted");
-            //     });
-            //     link.addEventListener("mouseleave", () => {
-            //         console.log('MOUSE LEAVE!!');
-            //         // referencedPost.classList.remove("highlighted");
-            //     });
-            // });
+//elem short for element
+function isElementVisibleInViewport (elem)
+{
+    let rect = elem.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
