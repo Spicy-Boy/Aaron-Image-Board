4ae4d77bd81698b7b1fe021111d84ddb25773386
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2024-04-14 22:15:39 -0400

    Stuck so hard on links... can't get DOM to work right

--- public/js/createReplyLinks.js
+++ public/js/createReplyLinks.js
@@ -6,17 +6,22 @@ let arrPostReplies = [...document.getElementsByClassName("post-reply")];
 const postIDs = arrPostReplies.map(post => post.id);
 postIDs.reverse();
 
-console.log(postIDs);
+//TESTER all postIDs vvv
+// console.log(postIDs);
 
 //temporary code to replace ">>" in links while iterating a post
 let temporaryLinkIdentifier = "mOoMinDUmPy";
 
 //iterate every single post
 arrPostReplies.forEach( (post, i) => {
+    //TESTER vv
+    // console.log(post);
+
     // iterate every postNo, adding hyperlinks (>>) and reply functionality to every post in the thread
-    for (postNo of postIDs)
+    for (let postNo of postIDs)
     {
-        // console.log(post.innerHTML);
+        //TESTER vv
+        // console.log('ITERATING OVER',postNo);
         
         if (post.innerHTML.includes("&gt;&gt;"+postNo))
         {
@@ -25,42 +30,30 @@ arrPostReplies.forEach( (post, i) => {
 
             post.innerHTML = post.innerHTML.replaceAll("&gt;&gt;"+postNo, `<a href="#${postNo}" class="referencing-${postNo} reply-link">${temporaryLinkIdentifier}${postNo}</a>`);
 
-            const referencedPost = arrPostReplies.find( otherPost => otherPost.id === postNo);
-
-            // console.log("referenced: ",referencedPost);
-
-            const modifiedLinks = document.querySelectorAll("a.referencing-"+postNo);
-
-            //add event listener to cause referenced post to change bgc when corresponding reply links are hovered
-            modifiedLinks.forEach( (link) => {
-                link.addEventListener("mouseenter", () => {
-                    console.log('MOUSE OVER!!');
-                    referencedPost.classList.add("highlighted");
-                });
-                link.addEventListener("mouseleave", () => {
-                    referencedPost.classList.remove("highlighted");
-                });
-                link.addEventListener("click", () => {
-                    let highElements = document.querySelectorAll('.perma-highlighted');
-
-                    console.log("high:",highElements);
-
-                    highElements.forEach( (element) => {
-                        element.classList.remove('perma-highlighted');
-                    });
-
-                    referencedPost.classList.add("perma-highlighted");
-                });
-            });
-            console.log("modified links for "+postNo+":,",modifiedLinks);
-
-            // BUGFIX NEEDED: start iterating postnos at the end since >>1 overrides >>12
-
-            // add a little window that pops up of the referenced post when hovering reply link :D
-
+            //TODO: add a little window that pops up of the referenced post when hovering reply link :D
             //TODO: greentext! Or red text, or whatever I want :)
         }
     }
 
     post.innerHTML = post.innerHTML.replaceAll(temporaryLinkIdentifier, "&gt;&gt;");
 });
+
+// DOESNT WORK vvv
+            // //the post that the link is referencing
+            // const referencedPost = arrPostReplies.find( otherPost => otherPost.id === postNo);
+            // //TESTER vv
+            // console.log(post.id+" references "+referencedPost.id);
+
+            // modifiedLinks = post.querySelectorAll(".referencing-"+postNo);
+            // console.log("modded links:",modifiedLinks);
+            // modifiedLinks.forEach( (link) => {
+            //     console.log(link);
+            //     link.addEventListener("mouseenter", () => {
+            //         console.log('MOUSE OVER!!');
+            //         // referencedPost.classList.add("highlighted");
+            //     });
+            //     link.addEventListener("mouseleave", () => {
+            //         console.log('MOUSE LEAVE!!');
+            //         // referencedPost.classList.remove("highlighted");
+            //     });
+            // });
