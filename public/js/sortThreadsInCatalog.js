7af887f2108a2546559046e09bb54e883cf8ee11
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2024-04-19 23:28:49 -0400

    Sorting on catalog complete

--- public/js/sortThreadsInCatalog.js
+++ public/js/sortThreadsInCatalog.js
@@ -1,11 +1,16 @@
-let threads = document.getElementsByClassName("thread-post");
+// !! this script is primarily called in views/partials/options-nav.ejs 
+const threads = document.getElementsByClassName("thread-post");
+const catalog = document.querySelector('.catalog');
+
+console.log(threads);
+
 
 // data variables in each thread-post
 /**
  * data-threadNo
  * data-dateOfMostRecentPost
  * data-numberOfPosts
- */
+**/
 
 let sortByDropdown = document.getElementById('sortByDropdown');
 
@@ -14,8 +19,47 @@ sortByDropdown.addEventListener('change', sortHtmlCollection);
 //example of accessing a data-variable
 // threads.div.dataset.threadNo
 
-console.log(threads);
 
 function sortHtmlCollection () {
-    
+    let threadsArray = [...threads];
+
+    if (this.value == "mostRecent")
+    {
+        // console.log('sorting by most recent activity');
+
+        threadsArray.sort( (a,b) => {
+            return parseInt(b.dataset.dateofmostrecentpost) - parseInt(a.dataset.dateofmostrecentpost);
+        });
+    }
+    else if (this.value == "mostReplies")
+    {
+        // console.log('sorting by most replies');
+
+        threadsArray.sort( (a,b) => {
+            return parseInt(b.dataset.numberofposts) - parseInt(a.dataset.numberofposts);
+        });
+    }
+    else if (this.value == "newest")
+    {
+        // console.log('sorting by newest');
+
+        threadsArray.sort( (a,b) => {
+            return parseInt(b.dataset.threadno) - parseInt(a.dataset.threadno);
+        });
+    }
+    else if (this.value == "oldest")
+    {
+        // console.log('sorting by newest');
+
+        threadsArray.sort( (a,b) => {
+            return parseInt(a.dataset.threadno) - parseInt(b.dataset.threadno);
+        });
+    }
+
+    //replace dom elements in catalog vvv to re-order 
+    threadsArray.forEach(thread => {
+        catalog.appendChild(thread);
+    });
+
+    console.log(threadsArray);
 }
