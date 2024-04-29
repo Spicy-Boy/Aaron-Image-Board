7d2e4e323a4da6fce788de446bc12f91ccb6a050
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2024-04-29 00:27:06 -0400

    Grow and shrink images with a click!!

--- 0.HTML Testing/enlargeImage.js
+++ 0.HTML Testing/enlargeImage.js
@@ -4,18 +4,41 @@
 let clickableImages = document.querySelectorAll(".image-enlargement");
 
 clickableImages.forEach((link) => {
+    let isDefaultSize = true;
+
     link.addEventListener("click", (e)=>{
         //TESTER vvv
         // console.log('Clicked image :)');
         let img = link.querySelector("img");
 
-        //used to ensure image doesn't exceed viewport
-        let viewportWidth = window.innerWidth;
-        let linkPosLeft = link.getBoundingClientRect().left;
-        let maxWidth = viewportWidth - linkPosLeft;
+        if (isDefaultSize)
+        {
+            //ensure image doesn't exceed viewport
+            let leftPosition = img.getBoundingClientRect().left;
+            let scrollbarWidth = window.innerWidth - document.body.clientWidth;
+            let maxWidth = window.innerWidth - leftPosition - scrollbarWidth;
 
-        img.style.maxWidth = Math.min(maxWidth, parseInt(link.dataset.imgWidth)) + "px";
-        img.style.maxHeight = link.dataset.imgHeight +"px";
+            //chooses the maxWidth set by screen limitation or max width of image, whichever is smaller!
+            img.style.maxWidth = Math.min(maxWidth, parseInt(link.dataset.imgWidth)) + "px";
+
+            img.style.maxHeight = link.dataset.imgHeight +"px";
+
+            img.style.marginBottom = "15px";
+            
+            isDefaultSize = false;
+        }
+        else 
+        {
+            img.style.maxWidth = "";
+
+            img.style.maxHeight = "";
+
+            img.style.marginBottom = "";
+
+            isDefaultSize = true;
+        }
+
+
 
         e.preventDefault();
     });
